ac586a325d9c0ecc67d0d25df974c66ecc85656a
Tamas Nepusz <ntamas@gmail.com>
2021-02-09 12:41:42 +0100

    updated build script so it works when the repo is embedded as a submodule

--- build-book.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-docker run --rm -it -v $(pwd):/documents/ asciidoctor/docker-asciidoctor \
-	asciidoctor-pdf \
-		-a icons=font \
-		-a imagesdir=./assets/images \
-		-a revdate=`date +%Y-%m-%d` \
-		-a pdf-theme=book-theme.yml \
-		book.adoc
